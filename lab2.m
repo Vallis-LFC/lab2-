4b54318f51337e39df9eea2cee1a5e931819af5b
Vallis-LFC <115032819+Vallis-LFC@users.noreply.github.com>
2025-10-19 17:14:00 -0400

    Update lab2.m

--- lab2.m
+++ lab2.m
@@ -1,7 +1,6 @@
 %A.1
 R = [1e4, 1e4, 1e4]; 
 C=[1e-6,1e-6]; %sets the componenet values
-C=[1e-9,1e-6];
 A = [1,(1/R(1)+1/R(2)+1/R(3))/C(2), 1/(R(1)*R(2)*C(1)*C(2))];   %determine the coeffecients for characteristic equation
 lambda = roots(A);
 
