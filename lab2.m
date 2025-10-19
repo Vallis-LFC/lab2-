3c6fcdd955f4e09e037b4a6ed8f0623e8f2d34e1
Vallis-LFC <115032819+Vallis-LFC@users.noreply.github.com>
2025-10-19 13:05:05 -0400

    D.2 and C.1 done

--- lab2.m
+++ lab2.m
@@ -15,3 +15,31 @@ plot(t,h(t));
 title("lambda");
 xlabel("t");
 ylabel("rad");
+
+%C.1
+t = [-1:0.001:5];
+
+u = @(t) 1.0.*(t>=0);
+h1 = @(t) exp(t/5).*u(t);
+h2 = @(t) 4*exp(-t/5).*u(t);
+h3 = @(t) 4*exp(-t).*u(t);
+h4 = @(t) 4*(exp(-t/5)-exp(-t)).*u(t);
+
+plot(t,h1(t));
+xlabel("t");
+ylabel("h(t)");
+hold on;
+
+plot(t,h2(t));
+plot(t,h3(t));
+plot(t,h4(t));
+legend('h1', 'h2', 'h3', 'h4');
+%plots all the graphs on the same figure
+hold off;
+
+%D.1
+%put in the calculations (hand or smth)
+
+%D.2
+%the duration of the convolution is equal to the sum of the duration of the
+%functions
