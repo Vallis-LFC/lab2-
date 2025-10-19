a209bc136e5aedaff7fa7c88a92beb6e54ea5b13
Vallis-LFC <115032819+Vallis-LFC@users.noreply.github.com>
2025-10-19 17:18:00 -0400

    Update lab2.m

--- lab2.m
+++ lab2.m
@@ -3,9 +3,10 @@ R = [1e4, 1e4, 1e4];
 C=[1e-6,1e-6]; %sets the componenet values
 A = [1,(1/R(1)+1/R(2)+1/R(3))/C(2), 1/(R(1)*R(2)*C(1)*C(2))];   %determine the coeffecients for characteristic equation
 lambda = roots(A);
+poly(lambda);
 
 %A.2
-poly(lambda);
+%reusing the A.1 code lol cus im lazy lke that
 t = [0:0.0005:0.1];
 u = @(t) 1.0*(t>=0);
 h = @(t) (C(1).*exp(lambda(1).*t)+C(2).*exp(lambda(2).*t)).*(u(t));
